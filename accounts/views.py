a5c34f5059a508e8c4e4332955c0f4285e37d074
компас <lychkiv18@gmail.com>
2024-07-04 19:26:39 +0300

    .

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.conf.urls import handler403
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from accounts.forms import UserLoginForm, UserRegisterForm
+from accounts.models import UserProfile, Role
+
+
+# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+    form_class = UserLoginForm
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('accounts:login')
+
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserRegisterForm
+    success_url = 'accounts:login'
+
+    def form_valid(self, form):
+        user = form.save()
+        default_role, created = Role.objects.get_or_create(name = 'User')
+        UserProfile.objects.create(user = user, role=default_role)
+        login(self.request, user)
+        return redirect(reverse_lazy('accounts:login'))
