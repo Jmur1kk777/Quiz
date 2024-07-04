5243b989654c5342adcf5aa1731f479f99a60c9e
компас <lychkiv18@gmail.com>
2024-07-04 20:31:54 +0300

    додав моделі до квіз

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from quizapp.models import Quiz, Question, Answer
 # Register your models here.
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
--- quizapp/models.py
+++ quizapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    QUESTION_TYPES = [
+        ("text","Text question"),
+        ("image", "Image question"),
+        ("video", "Video question")
+    ]
+
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    text = models.TextField()
+    type = models.CharField(max_length=20, choices=QUESTION_TYPES, default="text")
+    image = models.ImageField(upload_to='question_images/',blank=True, null=True)
+    video = models.FileField(upload_to='question_videos/',blank=True, null=True)
+    created = models.DateField(auto_now_add=True)
+    time_limit = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,related_name="answers")
+    text = models.TextField()
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
